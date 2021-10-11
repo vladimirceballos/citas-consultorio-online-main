29094eec0187be679fe3d879311c720ad8bb6774
vladimirceballos <89559341+vladimirceballos@users.noreply.github.com>
2021-10-10 23:44:44 -0500

    confirmar

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://jjbocsmdipegxk:3d71afcd6756199393a25037b52dd43d576770b44a15464c72947aec12865130@ec2-52-0-93-3.compute-1.amazonaws.com:5432/d7fvitj0ia8476'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
+
+
 db = SQLAlchemy(app)
 
 from rutas import *
@@ -13,4 +16,4 @@ from rutas import *
 if __name__ == '__main__':
     from rutas import *
 
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
